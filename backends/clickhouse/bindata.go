e4b42bfd57be7d319240a78c93560f828ea4ebd8
Ferran Borreguero <ferranbt@protonmail.com>
2021-07-21 09:48:58 +0200

    Fix events role permissions inside k8s

--- backends/clickhouse/bindata.go
+++ backends/clickhouse/bindata.go
@@ -93,7 +93,7 @@ func resourcesClusterTemplate() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "resources/cluster.template", size: 2140, mode: os.FileMode(436), modTime: time.Unix(1625669072, 0)}
+	info := bindataFileInfo{name: "resources/cluster.template", size: 2140, mode: os.FileMode(436), modTime: time.Unix(1626853656, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
