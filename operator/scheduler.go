82f6fa661efb23b5d8c5056f3b73798a01047fa8
Ferran Borreguero <ferranbt@protonmail.com>
2021-07-29 17:59:11 +0200

    Add e2e test to delete the cluster

--- operator/scheduler.go
+++ operator/scheduler.go
@@ -1,6 +1,8 @@
 package operator
 
 import (
+	"fmt"
+
 	gproto "github.com/golang/protobuf/proto"
 	"github.com/teseraio/ensemble/lib/uuid"
 	"github.com/teseraio/ensemble/operator/proto"
@@ -73,6 +75,9 @@ func (s *scheduler) Process(eval *proto.Evaluation) (*proto.Plan, error) {
 	for _, i := range r.res.ready {
 		ii := i.Copy()
 		ii.Canary = false
+
+		fmt.Println("-- ready --")
+		fmt.Println(ii.Status)
 
 		plan.NodeUpdate = append(plan.NodeUpdate, ii)
 	}
